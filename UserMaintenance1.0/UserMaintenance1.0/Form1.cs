aa4ced106af2ba6198caaa360c6bbb158f3c5514
barna <barnus.horvath11@gmail.com>
2022-10-02 21:42:50 +0200

    fájlba írás

--- UserMaintenance1.0/UserMaintenance1.0/Form1.cs
+++ UserMaintenance1.0/UserMaintenance1.0/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace UserMaintenance1._0
             InitializeComponent();
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;             
             listBox1.DataSource=users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -28,8 +30,28 @@ namespace UserMaintenance1._0
         private void button1_Click(object sender, EventArgs e)
         {
             var u = new User();
-            u.FullName = label1.Text;
+            u.FullName = textBox1.Text;
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "csv";
+            sfd.Filter = "Comma Separated Values (*.csv) | *.csv)"; 
+            sfd.InitialDirectory = Application.StartupPath;
+            if (sfd.ShowDialog() == DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                foreach (var s in users)
+                {
+                    sw.Write(s.id);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.Write(";");
+                    sw.WriteLine();
+                }
+                
         }
     }
 }
