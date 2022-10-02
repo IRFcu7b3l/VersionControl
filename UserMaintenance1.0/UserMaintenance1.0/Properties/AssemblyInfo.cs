ed884cd6864d8218384fb570433695720a20f324
barna <barnus.horvath11@gmail.com>
2022-10-02 20:18:44 +0200

    Initial commit reload

--- UserMaintenance1.0/UserMaintenance1.0/Properties/AssemblyInfo.cs
+++ UserMaintenance1.0/UserMaintenance1.0/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UserMaintenance")]
+[assembly: AssemblyTitle("UserMaintenance1.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UserMaintenance")]
+[assembly: AssemblyProduct("UserMaintenance1.0")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d64d4e5d-f3ca-4789-8576-dd050e5f4bcb")]
+[assembly: Guid("ab270105-69ea-439e-bbbd-60fbe2069824")]
 
 // Version information for an assembly consists of the following four values:
 //
