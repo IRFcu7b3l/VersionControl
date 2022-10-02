cf80c1e0fb0a6cf8f7b824204080a581fb3fdc73
barna <barnus.horvath11@gmail.com>
2022-10-02 20:36:15 +0200

    szia uram

---
+++ UserMaintenance1.0/UserMaintenance1.0/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance1._0.Entities
+{
+    internal class User
+    {
+        public Guid id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get {
+                return string.Format("{0} {1}", FirstName, LastName);
+                    } 
+        }
+    }
+}
