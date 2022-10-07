95ab200849b6442c51ff6b9f1aff1a91476782eb
barna <barnus.horvath11@gmail.com>
2022-10-07 10:52:39 +0200

    ORM

--- 04_cu7b3l/04_cu7b3l/Form1.cs
+++ 04_cu7b3l/04_cu7b3l/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace _04_cu7b3l
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats;
+        RealEstateEntities context=new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        void LoadData() {
+            flats = context.Flats.ToList();
         }
     }
 }
