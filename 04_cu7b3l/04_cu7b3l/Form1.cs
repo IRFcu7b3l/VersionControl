f73d1c00e2a47e54ce50dbe5b08bc6b6d5f36564
barna <barnus.horvath11@gmail.com>
2022-10-07 11:03:41 +0200

    Excel munkafüzet létrehozása

--- 04_cu7b3l/04_cu7b3l/Form1.cs
+++ 04_cu7b3l/04_cu7b3l/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace _04_cu7b3l
 {
@@ -16,13 +17,36 @@ namespace _04_cu7b3l
     {
         List<Flat> flats;
         RealEstateEntities context=new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = new Excel.Workbook();
+                xlSheet = new Excel.Worksheet();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg=string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlWB.Close();
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
         void LoadData() {
             flats = context.Flats.ToList();
         }
+        
     }
 }
