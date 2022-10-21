5b45080169a686bdae96ddea64a201ae7ef97083
barna <barnus.horvath11@gmail.com>
2022-10-21 10:42:07 +0200

    Árfolyam lista

--- cu7b3l_5/cu7b3l_week5/Form1.cs
+++ cu7b3l_5/cu7b3l_week5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using cu7b3l_week5.MnbServiceReference;
+﻿using cu7b3l_week5.Entities;
+using cu7b3l_week5.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,12 @@ namespace cu7b3l_week5
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates=new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            dataGridView1.DataSource = Rates;
             
         }
         void GetExchangeRates() {
