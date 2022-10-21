775bc51dd2c282d41476fe1e65fa70c5b47fa4b8
barna <barnus.horvath11@gmail.com>
2022-10-21 10:27:59 +0200

    Service Reference

--- cu7b3l_5/cu7b3l_week5/Form1.cs
+++ cu7b3l_5/cu7b3l_week5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cu7b3l_week5.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,13 @@ namespace cu7b3l_week5
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+            
         }
     }
+    
 }
