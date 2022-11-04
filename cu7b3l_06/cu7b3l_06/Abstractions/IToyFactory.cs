dd4440a50bc5231b1f896e52b1f709e4d4c63289
barna <barnus.horvath11@gmail.com>
2022-11-04 16:33:23 +0100

    gyár absztrakció kezelése

---
+++ cu7b3l_06/cu7b3l_06/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu7b3l_06.Abstractions
+{
+    internal interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
