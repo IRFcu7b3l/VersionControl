b4883f3b6df29cc59f6f34cb0a6df3cc151a8f2c
barna <barnus.horvath11@gmail.com>
2022-11-04 11:18:59 +0100

    autó létrehozása

---
+++ cu7b3l_06/cu7b3l_06/Car.cs
@@ -0,0 +1,19 @@
+﻿using cu7b3l_06.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu7b3l_06
+{
+    internal class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
