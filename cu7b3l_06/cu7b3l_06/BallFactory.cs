2d734b54275d5d3375313b19d214f90acc7908b9
barna <barnus.horvath11@gmail.com>
2022-11-04 10:32:33 +0100

    labdagyár

---
+++ cu7b3l_06/cu7b3l_06/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu7b3l_06
+{
+    internal class BallFactory
+    {
+
+        public Ball CreateNew() {
+            return new Ball();
+        
+        }
+    }
+}
