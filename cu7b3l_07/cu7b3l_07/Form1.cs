df65090f0457c75bb46ca73467c15963fedcef9f
barna <barnus.horvath11@gmail.com>
2022-11-11 10:53:56 +0100

    portfólió tárolása, portfólió érték kiszámítása, VaR kiszámítása

--- cu7b3l_07/cu7b3l_07/Form1.cs
+++ cu7b3l_07/cu7b3l_07/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cu7b3l_07.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,11 +15,49 @@ namespace cu7b3l_07
     {
         List<Tick> ticks;
         PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             ticks = context.Ticks.ToList();
-            dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervallum = 30;
+            DateTime kezdőDátum = (from x in ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i <z.Days-intervallum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervallum))
+                    - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i+" "+ny);
+
+            }
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x).ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+
+        }
+        private void CreatePortfolio() {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+        private decimal GetPortfolioValue(DateTime date) {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in ticks
+                            where item.Index == x.Index.Trim()
+                            && date <= x.TradingDay
+                            select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
