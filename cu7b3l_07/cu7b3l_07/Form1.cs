ccb8d0901069c565858b06a85b19a282338ec2d6
barna <barnus.horvath11@gmail.com>
2022-11-11 11:04:57 +0100

    fájlba írás

--- cu7b3l_07/cu7b3l_07/Form1.cs
+++ cu7b3l_07/cu7b3l_07/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +17,14 @@ namespace cu7b3l_07
         List<Tick> ticks;
         PortfolioEntities context = new PortfolioEntities();
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> nyereségekRendezve = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
             ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = ticks;
             CreatePortfolio();
-            List<decimal> Nyereségek = new List<decimal>();
+            List<decimal> Nyereségek = new List<decimal>();          
             int intervallum = 30;
             DateTime kezdőDátum = (from x in ticks select x.TradingDay).Min();
             DateTime záróDátum = new DateTime(2016, 12, 30);
@@ -34,7 +37,7 @@ namespace cu7b3l_07
                 Console.WriteLine(i+" "+ny);
 
             }
-            var nyereségekRendezve = (from x in Nyereségek
+            nyereségekRendezve = (from x in Nyereségek
                                       orderby x
                                       select x).ToList();
             MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
@@ -58,6 +61,28 @@ namespace cu7b3l_07
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Separated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog()!=DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8)) {
+                    sw.WriteLine();
+                    foreach (var ny in nyereségekRendezve)
+                    {
+                       // sw.Write(ny.)
+                    }
+                
+                } 
+            }
         }
     }
 }
