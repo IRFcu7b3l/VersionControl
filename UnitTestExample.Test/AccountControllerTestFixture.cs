9eaa4e5253393c546628a973bd59c2e57936c5ba
barna <barnus.horvath11@gmail.com>
2022-11-25 10:40:40 +0100

    email validálás teszt kifejtése

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
         [Test]
-        void TestValidateEmail() { }
+        void TestValidateEmail(string email, bool expectedResult) {
+            //Arrange
+            var accountController = new AccountController();
+            //Act
+            var actualResult = accountController.ValidateEmail(email);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
