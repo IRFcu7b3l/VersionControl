e8c1d7b95740d9efbcaef257d3e72a330255e4d6
barna <barnus.horvath11@gmail.com>
2022-12-02 10:41:58 +0100

    osztályok létrehozása

---
+++ cu7b3l_09/cu7b3l_09/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu7b3l_09.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
